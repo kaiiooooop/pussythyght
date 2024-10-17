73dc61b88ed59122cc35ce48fd24f0c2479beeff
kaiiooooop <160140295+kaiiooooop@users.noreply.github.com>
2024-10-17 11:31:37 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,41 @@
+criaCartao(
+    'Musica',
+    'Quantos grammys Madonna tem?',
+    'Wla tem um total de 25 grammys'
+)
+
+criaCartao(
+    'Musica',
+    'Quais artistas foram principais vencedores do Gramm em 2023 e quais categorias eles se destacaram?',
+    'Entre os destaques, Beyonce foi a grande vencedora, levando o prêmio de álbum do ano por "renaissance". Outros vencedoram notaveis também são: Lizzo, Harry Styles e Taylor Swift'
+)
+
+criaCartao(
+    'Musica',
+    'Como a musica "blinding lights" de The Weeknd impactou as paradas musicais e a industria pop?',
+    'A música e The Weeknd se tornou um fenõmeno, permanecendo nas paradas por mais de um ano e estabelecendo recordes de tempo nas paradas da Billbord'
+)
+
+criaCartao(
+    'Musica',
+    'Quais as principais colaboraçoẽs entre artistas pop que foram lançadas em 2024',
+    'AS principais coloboraçoẽs são: "fortnight" de Tayor Swift com Post Malone, "Dance Again" de Dua lipa com SZA, "Summer Nights" de Ed Sheeran com Justin Bieber'
+)
+
+criaCartao(
+    'Musica',
+    'COmo a presença de artistas latinos como Shakira e Bad Bunny, tem influenciado o cenário da musica pop global?',
+    'Eles têm desempenhado um papel crucial na globalização da musica pop, trazendo ritmos latinos para o mainstream.'
+)
+
+criaCartao(
+    'Musica',
+    'Quais tendências de moda e estilo musical emergiram entre os artistas pop nos ultimos anos',
+    'Nos ultimos anos tem visto uma mistura de estética retro com streetwear nas apresentaçoẽs e clipes de artistas.'
+)
+
+criaCartao(
+    'Musica',
+    'Como as platamformas de streaming mudaram a forma que os artistas promoveram musicas e se concetaram com os fãs?',
+    'As plataformas de straming revulucionaram a promoção musical, permitindo que artistas se conectem diretamente com os fãs, atraves de musica, playlist e albuns'
+)
